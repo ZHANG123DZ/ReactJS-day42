b2905b95bb34ab98126a3bc27201e32a6c30e077
zhang <hoangquangthang999@gmail.com>
2025-03-16 23:20:51 +0700

    day42

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import vercel from '../vercel.json'
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
